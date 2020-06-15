82b07ec360ca47ea83efcd5d1134579943fea8a0
eishun <shutainer@yahoo.co.jp>
2020-06-15 23:31:26 +0900

    Initial Commit

---
+++ Swift5MapAndProtocol_2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5MapAndProtocol_2
+//
+//  Created by 須藤英隼 on 2020/06/15.
+//  Copyright © 2020 Eishun Sudo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
